160209ef426223b25022a12afd270a2ecdfb6a41
Diego Navarro <dnmellen@gmail.com>
2024-08-12 15:44:51 +0200

    Add nvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  config = function()
+    require("venv-selector").setup({
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+    })
+  end,
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { "<leader>vs", "<cmd>VenvSelect<cr>" },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+  },
+}
